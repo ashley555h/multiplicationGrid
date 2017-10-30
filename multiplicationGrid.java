55b9b01b32c18dd6be14a86c5a7b0ce08fb05f93
ashley555h <ahudgens@clearent.com>
2017-10-29 19:15:42 -0500

    Update multiplicationGrid.java

--- multiplicationGrid.java
+++ multiplicationGrid.java
@@ -1,27 +1,31 @@
 import static java.lang.System.out;
 import java.util.Scanner;
 
-public class multiplicationGrid
-{
-    public static void main(String[] args)
-    {
-        Scanner keyboard = new Scanner(System.in);
-        int sizeOfTable;
+public class multiplicationTable {
+    public static void main(String[] args) {
+        Scanner keyboard= new Scanner(System.in);
+        int tableSize;
+        System.out.print("How big would you like the table? ");
+        tableSize = keyboard.nextInt();
+        if (tableSize > 20) {
+            System.out.println("Invalid");
+        } else {
+            printMultiplicationTable(tableSize);
+        }
+    }
 
-        out.println("Welcome to Multiplication Tables");
-        out.println("");
-        out.println("How large would you like to see them?");
-        sizeOfTable = keyboard.nextInt();
-
-        for (int a = 0; a <= sizeOfTable; a++)
-        {
-            for (int b = 0; b <= sizeOfTable; b++)
-            {
-                System.out.print((a*b) + "\t");
+    public static void printMultiplicationTable(int tableSize) {
+        System.out.format("      ");
+        for(int i = 0; i<=tableSize; i++) {
+            System.out.format("\t",i);
+        }
+        System.out.println();
+        for(int i = 0; i <= tableSize; i++) {
+            System.out.format("%4d",i);
+            for(int j = 0;j <= tableSize; j++) {
+                System.out.format("%4d",i*j);
             }
             System.out.println();
         }
     }
-
-
 }
